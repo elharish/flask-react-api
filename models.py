5638089f715d3ed8010d2699c9c12b9bdbe0001a
harish <atpcs033@outlook.com>
2026-06-12 12:25:06 +0530

    a

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(120), nullable=False)
-    mobile = db.Column(db.String(10), nullable=False)
+    phone = db.Column(db.String(10), nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
